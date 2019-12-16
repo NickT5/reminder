a8e6254c90e39c10cf18e1fea2ca6114f8f73577
Nick Todts <nick-todts@hotmail.com>
2019-12-16 21:22:10 +0100

    Add flask_bootstrap package. Extend from bootstrap/base.html instead of layout.html to use Bootstrap in a html page.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, url_for
+from flask_bootstrap import Bootstrap
 
 # Configure app
 app = Flask(__name__)
+Bootstrap(app)
 
 # All tasks
 tasks = ["dishes", "fitness"]
@@ -15,3 +17,7 @@ def index():
 @app.route("/tasks", methods=["POST"])
 def task():
     return render_template("tasks.html", tasks=tasks)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
